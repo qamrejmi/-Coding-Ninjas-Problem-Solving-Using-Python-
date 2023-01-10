6d2817d2726ef1d8a56a62cb9ad639bf5ab96246
qamrejmi <98503074+qamrejmi@users.noreply.github.com>
2023-01-10 21:27:37 +0530

    Add files via upload

---
+++ git_hub.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Find average Marks
+# 
+
+# In[3]:
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+avg=(a+b+c)/3
+print(avg)
+
+
+# # Find X raised to power N
+# 
+
+# In[4]:
+
+
+base=int(input())
+pwr=int(input())
+c=base**pwr
+print(c)
+
+
+# # Arithmetic Progression
+
+# In[5]:
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+c_diff=b-a
+print(c_diff)
+
+
+# # Rectangular Area
+
+# In[6]:
+
+
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+Area=(x2-x1)*(y2-y1)
+print(Area)
+
+
+# In[ ]:
+
+
+
+
